6ef211da1a21513b94f5a7a13396d464f93937d8
alessandro.rodrigues <alessandro.rodrigues@ifto.edu.br>
2019-12-10 18:07:11 -0300

    Initial commit

--- to_do/urls.py
+++ to_do/urls.py
@@ -3,7 +3,6 @@ from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from api import views
-from to_do.views import users
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,6 +10,5 @@ router.register(r'tasks', views.TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('client/users', users)
     #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
