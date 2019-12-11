84ab720c67db9fac9750978511b07acc589562e7
alessandro.rodrigues <alessandro.rodrigues@ifto.edu.br>
2019-12-11 08:40:10 -0300

    Enabling CORS

--- to_do/settings.py
+++ to_do/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+CORS_ORIGIN_WHITELIST = (
+    'https://to-do-api-alessandro.herokuapp.com',
+)
