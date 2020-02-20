b0650736a64ec222dbdd79fbd9b24f0293ff0a96
Alessandro <alessandro.rodrigues@ifto.edu.br>
2020-02-20 14:20:33 -0300

    correct testcase

--- api/tests.py
+++ api/tests.py
@@ -135,4 +135,4 @@ class TaskTest(TestCase):
         # get API response
         client = Client()
         response = client.get("/tasks/user/10/")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
