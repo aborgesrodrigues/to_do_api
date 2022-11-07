eb04c84fee472477d0c3665825b17d4f358a1a3a
Alessandro Rodrigues <alessandro.rodrigues@koalafi.com>
2022-11-07 15:42:12 -0300

    fix to work on newer versions

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.urls import path, include
 from rest_framework import routers
 from drf_yasg.views import get_schema_view
@@ -19,8 +19,8 @@ schema_view = get_schema_view(
 )
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet, base_name='users')
-router.register(r'tasks', views.TaskViewSet, base_name='tasks')
+router.register(r'users', views.UserViewSet, basename='users')
+router.register(r'tasks', views.TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
